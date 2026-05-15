35dba35e92bea2bf7aa493dd6e7313058560bebe
leema2196 <leemajeyarosy96@gmail.com>
2026-05-15 12:50:22 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ class main
 {
 public static void main(String args[])
 {
-System.out.println("student management system inside student-feature")
+System.out.println("student management system ")
 }
 }
